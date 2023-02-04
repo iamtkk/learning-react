2c7b137eb6ed7f1c06a18183a44e7a94834d952f
iamtkk <iamtkk2@gmail.com>
2023-02-04 11:29:50 +0900

    redux

---
+++ playground/react-redux-tutorial/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+import reportWebVitals from './reportWebVitals';
+// import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
