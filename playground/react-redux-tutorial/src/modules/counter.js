520bdeff57c83e6f7ac52ddc940fbc7dd578d6e1
iamtkk <iamtkk2@gmail.com>
2023-02-04 22:26:49 +0900

    1

--- playground/react-redux-tutorial/src/modules/counter.js
+++ playground/react-redux-tutorial/src/modules/counter.js
@@ -1,26 +1,38 @@
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+import { createAction, handleAction } from "redux-actions";
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
 
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleAction(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
